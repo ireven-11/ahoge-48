6da81a01dec1a0b3d6c4c0dc1f98fe37937f2a57
ireven <hensati11@gmail.com>
2025-11-22 16:27:07 +0900

    足場を作ろうとした

--- ahoge-48/Stage.cpp
+++ ahoge-48/Stage.cpp
@@ -8,30 +8,51 @@ Stage::Stage()
 	skypos1_		= VGet(0.0f, 0.0f, 0.0f);
 	skypos2_		= VGet(0.0f, -sky_height + 1.0f, 0.0f);
 	scaffoldGraph_	= LoadGraph("graph/tileset.png");
+	for (int i = 0; i < max_scaffold_number; i++)
+	{
+		scaffoldPos_[i] = VGet(0.0f, 0.0f, 0.0f);
+		scaffoldGraphArray_[i] = scaffoldGraph_;
+	}
+	scaffoldCounter_ = 0;
 }
 
 Stage::~Stage()
 {
 	DeleteGraph(waterGraph_);
 	DeleteGraph(skyGraph_);
+	for (int i = 0; i < max_scaffold_number; i++)
+	{
+		DeleteGraph(scaffoldGraphArray_[i]);
+	}
 }
 
 void Stage::init()
 {
 	skypos1_ = VGet(0.0f, 0.0f, 0.0f);
 	skypos2_ = VGet(0.0f, -sky_height, 0.0f);
-
+	for (int i = 0; i < max_scaffold_number; i++)
+	{
+		scaffoldPos_[i] = VGet(0.0f, 0.0f, 0.0f);
+	}
+	scaffoldCounter_ = 0;
 }
 
 void Stage::update()
 {
 	updateSky();
+	updateScaffold();
 }
 
 void Stage::draw()const
 {
 	DrawExtendGraph(skypos1_.x, skypos1_.y, skypos1_.x + sky_widht, skypos1_.y + sky_height, skyGraph_, TRUE);
 	DrawExtendGraph(skypos2_.x, skypos2_.y, skypos2_.x + sky_widht, skypos2_.y + sky_height, skyGraph_, TRUE);
+
+	for (int i = 0; i < max_scaffold_number; i++)
+	{
+		DrawExtendGraph(scaffoldPos_[i].x, scaffoldPos_[i].y, scaffoldPos_[i].x + scaffold_widht, scaffoldPos_[i].y + scaffold_height,
+			scaffoldGraph_, TRUE);
+	}
 
 	DrawExtendGraph(water_position.x, water_position.y, water_position.x + water_width, water_position.y + water_height,
 		waterGraph_, TRUE);
@@ -49,5 +70,26 @@ void Stage::updateSky()
 	else if (skypos2_.y > sky_height)
 	{
 		skypos2_.y = -sky_height;
+	}
+}
+
+void Stage::updateScaffold()
+{
+	for (int i = 0; i < max_scaffold_number; i++)
+	{
+		scaffoldPos_[i].y += scaffold_fall_spped;
+	}
+
+	++scaffoldCounter_;
+	if (scaffoldCounter_ < max_scaffold_Count) return;
+
+	scaffoldCounter_ = 0;
+	for (int i = 0; i < max_scaffold_number; i++)
+	{
+		if (scaffoldPos_[i].y > 1080 || scaffoldPos_[i].y < 0) 
+		{
+			scaffoldPos_[i].y = init_scaffold_pos_y;
+			scaffoldPos_[i].x = static_cast<float>(SRand(1920) - scaffold_widht / 2);
+		}
 	}
 }
--- ahoge-48/Stage.h
+++ ahoge-48/Stage.h
@@ -2,7 +2,9 @@
 
 constexpr	float	gravity				= 0.6f;
 const		VECTOR	water_position		= VGet(0.0f, 700.0f, 0.0f);
-const		short	max_scaffold_number = 10;
+constexpr	short	max_scaffold_number = 10;
+constexpr	float	scaffold_widht		= 200.0f;
+constexpr	float	scaffold_height		= 75.0f;
 
 class Stage
 {
@@ -16,17 +18,23 @@ public:
 
 private:
 	void updateSky();
+	void updateScaffold();
 
 	int		waterGraph_;
 	int		skyGraph_;
 	VECTOR	skypos1_;
 	VECTOR	skypos2_;
 	int		scaffoldGraph_;
+	int		scaffoldGraphArray_[max_scaffold_number];
 	VECTOR	scaffoldPos_[max_scaffold_number];
+	short	scaffoldCounter_;
 
-	const float		water_width		= 1920.0f;
-	const float		water_height	= 1080.0f;
-	const float		sky_widht		= 1920.0f;
-	const float		sky_height		= 1080.0f;
-	const float		scroll_speed	= 1.0f;
+	const float		water_width			= 1920.0f;
+	const float		water_height		= 1080.0f;
+	const float		sky_widht			= 1920.0f;
+	const float		sky_height			= 1080.0f;
+	const float		scroll_speed		= 1.0f;
+	const float		init_scaffold_pos_y	= -20.0f;
+	const short		max_scaffold_Count	= 200;
+	const float		scaffold_fall_spped = 5.0f;
 };
